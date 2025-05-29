bc5c3e12539cb14fdad7525d4a6c2c07b0111aa3
alineAssuncao <aline.jassuncao@gmail.com>
2025-05-28 23:28:01 -0300

    atualizações

--- src/main/java/br/rest/olaMundoTest.java
+++ src/main/java/br/rest/olaMundoTest.java
@@ -3,9 +3,18 @@ package br.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
 import org.junit.Assert;
 import org.junit.Test;
 import io.restassured.response.Response;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 
 public class olaMundoTest {
@@ -30,6 +39,49 @@ public class olaMundoTest {
         validacao.statusCode(200);
 
         RestAssured.get("https://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        //Give, then, when
+        RestAssured
+                .given()//Pré-condições
+                .when()//Ações
+                    .get("https://restapi.wcaquino.me/ola")
+                .then()//Verificações
+                    .statusCode(200);
     }
+
+    @Test
+    public void devoConhecerMatchersHamcrest(){
+        assertThat("Maria", Matchers.is("Maria"));
+        assertThat(128, Matchers.is(128));
+        assertThat(128, Matchers.isA(Integer.class));
+        assertThat(128d, Matchers.isA(Double.class));
+        assertThat(128d, Matchers.greaterThan(120d));
+        assertThat(128d, Matchers.lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1,2,3,5,8);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1,2,3,5,8));
+        assertThat(impares, containsInAnyOrder(1,5,2,3,8));
+        assertThat(impares, hasItem(2));
+        assertThat(impares, hasItems(2,8));
+
+        assertThat("Maria", Matchers.is(not("Joana")));
+        assertThat("Maria", Matchers.anyOf(is("Maria"), is("Ana")));
+        assertThat("Joaquina", allOf(startsWith("Jo"), endsWith("na"), containsString("qui")));
+    }
+
+    @Test
+    public void devoValidarBody(){
+        RestAssured
+                .given()
+                .when()
+                .get("https://restapi.wcaquino.me/ola")
+                .then()
+                .statusCode(200)
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(not(nullValue())));
+    }
+    //https://restapi.wcaquino.me/users/1
 
 }
