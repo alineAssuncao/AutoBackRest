03fd20d0db8111bf4fee56198f951cecb8d95075
alineAssuncao <aline.jassuncao@gmail.com>
2025-05-29 23:52:55 -0300

    atualizações

--- src/main/java/br/rest/userJsonTest.java
+++ src/main/java/br/rest/userJsonTest.java
@@ -5,9 +5,11 @@ import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.http.Method;
+import org.hamcrest.collection.IsArrayWithSize;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.hamcrest.Matchers.*;
@@ -105,5 +107,51 @@ public class userJsonTest {
                     .body("filhos.name", hasItem(Arrays.asList("Zezinho", "Luizinho")))
                     .body("salary", contains(1234.5678f, 2500, null))
         ;
+    }
+
+    @Test
+    public void deveFazerVerificacoesAvançadas(){
+        RestAssured
+                .given()
+                .when()
+                    .get("https://restapi.wcaquino.me/users")
+                .then()
+                    .statusCode(200)
+                    .body("", hasSize(3))
+                    .body("age.findAll{it <= 25}.size()", is(2))
+                    .body("age.findAll{it <= 25 && it > 20}.size()", is(1))
+                    .body("findAll{it.age <= 25 && it.age > 20}.name", hasItem("Maria Joaquina"))
+                    .body("findAll{it.age <= 25}[0].name", is("Maria Joaquina"))
+                    .body("findAll{it.age <= 25}[-1].name", is("Ana Júlia"))
+                    .body("find{it.age <= 25}.name", is("Maria Joaquina"))
+                    .body("findAll{it.name.contains('n')}.name", hasItems("Maria Joaquina", "Ana Júlia"))
+                    .body("findAll{it.name.length() > 10}.name", hasItems("João da Silva", "Maria Joaquina"))
+                    .body("name.collect{it.toUpperCase()}", hasItems("MARIA JOAQUINA"))
+                    .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}", hasItems("MARIA JOAQUINA"))
+                    .body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+                    .body("age.collect{it * 2}", hasItems(60, 50, 40))
+                    .body("id.max()", is(3))
+                    .body("salary.min()", is(1234.5678f))
+                    .body("salary.findAll{it != null}.sum()", is(closeTo(3734.5678f, 0.001)))
+                    .body("salary.findAll{it != null}.sum()", allOf(greaterThan(3000d), lessThan(4000d)))
+        ;
+    }
+
+    @Test
+    public void devoUnirJsonPathComJava(){
+        ArrayList<String> names =
+        RestAssured
+                .given()
+                .when()
+                    .get("https://restapi.wcaquino.me/users")
+                .then()
+                    .statusCode(200)
+                    .extract().path("name.findAll{it.startsWith('Maria')}")
+                //linha acima foi feita com os dados acima
+                //.body("name.findAll{it.startsWith('Maria')}.collect{it.toUpperCase()}.toArray()", allOf(arrayContaining("MARIA JOAQUINA"), arrayWithSize(1)))
+        ;
+        Assert.assertEquals(1, names.size());
+        Assert.assertTrue(names.get(0).equalsIgnoreCase("MaRIa joaQUIna"));
+        Assert.assertEquals(names.get(0).toUpperCase(), "maria joaquina".toUpperCase());
     }
 }
